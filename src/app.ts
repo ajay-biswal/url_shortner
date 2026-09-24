7527e0f7b745ce1cddeee2f2c34daf1434e604da
Ajay Biswal <116955513+ajay-biswal@users.noreply.github.com>
2026-09-25 04:10:56 +0530

    feat: mount auth and workspace routes

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express from "express";
-import urlRoutes from './routes/url.routes.js';
+import authRoutes from "./routes/auth.routes.js";
+import workspaceRoutes from "./routes/workspace.routes.js";
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/health', (_req, res)=>{
-    res.status(200).json({
-        status: "ok"
-    });
+app.get("/health", (_req, res) => {
+  res.status(200).json({
+    status: "ok",
+  });
 });
 
-app.use("/api/urls", urlRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/workspaces", workspaceRoutes);
 
 export default app;
